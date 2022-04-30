c500f16c58f8e1a595cb3a5ae5f2fa6e880269be
ritur28 <42659586+ritur28@users.noreply.github.com>
2022-04-30 18:33:12 -0400

    Update models.py

--- coffeemanager/models.py
+++ coffeemanager/models.py
@@ -4,19 +4,22 @@ from django.db import models
 
 
 class Customer(models.Model):
+    order_id = models.DecimalField(max_digits=8, decimal_places=4)
     name = models.CharField(max_length=45)
     email = models.CharField(max_length=45, primary_key=True)
 
     def __str__(self):
-        return "Name: "+self.name+" Email: "+self.email
+        return "Order ID: "+self.order_id+" Name: "+self.name+" Email: "+self.email
 
 
 class Drink(models.Model):
+    drink_id = models.DecimalField(max_digits=8, decimal_places=4)
     name = models.CharField(max_length=45)
     price = models.DecimalField(max_digits=8, decimal_places=4)
 
     def __str__(self):
-        return "Name: "+self.name+" Price: "+self.price
+        return "Drink ID: "+self.drink_id+" Name: "+self.name+" Price: "+self.price
+    
     
 class Shop(models.Model):
     shop_id = models.DecimalField(max_digits=8, decimal_places=4)
@@ -31,6 +34,7 @@ class Shop(models.Model):
                     self.address+" Phone Number: "+self.phone_num+" Opening Time: "+self.opening_time+
                     " Closing Time: "+self.closing_time
 
+            
 class Order(models.Model):
     order_id = models.DecimalField(max_digits=8, decimal_places=4)
     customer_id = models.DecimalField(max_digits=8, decimal_places=4)
@@ -40,7 +44,7 @@ class Order(models.Model):
         return "Order ID: "+self.order_id+" Customer ID: "+self.customer_id+" Order Status: "
                 +self.order_status
     
-
+    
 class OrderItem(models.Model):
     product_id = models.DecimalField(max_digits=8, decimal_places=4)
     quantity = models.DecimalField(max_digits=4, decimal_places=0)
@@ -48,6 +52,7 @@ class OrderItem(models.Model):
     
     def __str__(self):
         return "Product ID: "+self.product_id+" Quantity: "+self.quantity+" Order ID: "+self.order_id
+    
     
 class Review(models.Model):
     review_id = models.DecimalField(max_digits=8, decimal_places=4)
@@ -57,12 +62,14 @@ class Review(models.Model):
     def __str__(self):
         return "Review ID: "+self.review_id+" Number of Stars: "+self.numStars+" Comment: "+self.comment
     
+    
 class Cart(models.Model):
     cart_id = models.DecimalField(max_digits=8, decimal_places=4)
     customer_email = models.CharField(max_length=45, primary_key=True)
     
     def __str__(self):
         return "Card ID: "+self.cart_id+" Customer Email: "+self.customer_email
+    
     
 class CartEntry(models.Model):
     cart_id = models.DecimalField(max_digits=8, decimal_places=4)
